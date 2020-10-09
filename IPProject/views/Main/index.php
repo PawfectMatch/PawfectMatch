41a9fc358eabff3261922406654597c6a4b76c1a
manjirik23 <62017046+manjirik23@users.noreply.github.com>
2020-10-09 14:07:28 +0530

    Changed css of index page
    Changed css of Index page

--- IPProject/views/Main/index.php
+++ IPProject/views/Main/index.php
@@ -17,7 +17,7 @@
     <!-- NEWS PANEl -->
                 <td>
                     <div>
-                        <h1 style="color: aqua;">NEWS AND OTHER THINGIES</h1>
+                        <h1 style="color: #1d1e1f;">NEWS AND OTHER THINGS</h1>
                     </div>
                 </td>
 	<!-- GRID PANEL -->
@@ -25,49 +25,59 @@
                 <td class="GridPics">
                     
                     <div class="card">
-                        <img src="../../Public/Images/German-Shepherd.jpg" alt="John" style="width:100%">
-                        <h1>John Doe</h1>
-                        <p class="CardName">CEO & Founder, Example</p>
-                        <p>Harvard University</p>
+                        <img src="../../Public/Images/Whiskeyyyy.jpg" alt="Whiskey" style="width:100%">
+                        <h1>Whiskey Raut</h1>
+                        <p class="CardName">A cute white active Pomeranian</p>
+                        <p>4 years</p>
 
-                        <p><button>Contact</button></p>
+                        <p><button>Read more</button></p>
                     </div>
 
                     <div class="card">
-                        <img src="../../Public/Images/husky.jpg" alt="John" style="width:100%">
-                        <h1>John Doe</h1>
-                        <p class="CardName">CEO & Founder, Example</p>
-                        <p>Harvard University</p>
+                        <img src="../../Public/Images/Asterixx.jpg" alt="Asterix" style="width:100%">
+                        <h1>Asterix Dcosta</h1>
+                        <p class="CardName">Young, playful and happy Cocker Spaniel</p>
+                        <p>3 years</p>
 
-                        <p><button>Contact</button></p>
+                        <p><button>Read more</button></p>
                     </div>
 
                     <div class="card">
-                        <img src="../../Public/Images/German-Shepherd.jpg" alt="John" style="width:100%">
-                        <h1>John Doe</h1>
-                        <p class="CardName">CEO & Founder, Example</p>
-                        <p>Harvard University</p>
+                        <img src="../../Public/Images/BruceKherdekar.jpg" alt="Bruce" style="width:100%">
+                        <h1>Bruce Kherdekar</h1>
+                        <p class="CardName">Friendly, fluffy and fit Labrador Retriever</p>
+                        <p>4 years</p>
 
-                        <p><button>Contact</button></p>
+                        <p><button>Read more</button></p>
                     </div>
 
                     <div class="card">
-                        <img src="../../Public/Images/rottweiler-puppies.jpg" alt="John" style="width:100%">
-                        <h1>John Doe</h1>
-                        <p class="CardName">CEO & Founder, Example</p>
-                        <p>Harvard University</p>
+                        <img src="../../Public/Images/LeoN.jpg" alt="Leo" style="width:100%">
+                        <h1>Leo Nautiyal</h1>
+                        <p class="CardName">A sporty happy Labrador Retriever</p>
+                        <p>3 years</p>
 
-                        <p><button>Contact</button></p>
+                        <p><button>Read more</button></p>
                     </div>
 
                     <div class="card">
-                        <img src="../../Public/Images/Saint-bernard.jpg" alt="John" style="width:100%">
-                        <h1>John Doe</h1>
-                        <p class="CardName">CEO & Founder, Example</p>
-                        <p>Harvard University</p>
+                        <img src="../../Public/Images/tuffy.jpg" alt="Tuffy" style="width:100%">
+                        <h1>Tuffy</h1>
+                        <p class="CardName">A beautiful mixture of Labrador and Husky</p>
+                        <p>2 years</p>
 
-                        <p><button>Contact</button></p>
-                    </div>                    
+                        <p><button>Read more</button></p>
+                    </div> 
+                    
+                    
+                    <div class="card">
+                        <img src="../../Public/Images/Hachiko.jpg" alt="Hachiko" style="width:100%">
+                        <h1>Hachiko</h1>
+                        <p class="CardName">A very loyal and friendly Hachiko </p>
+                        <p>5 years</p>
+
+                        <p><button>Read more</button></p>
+                    </div>
 
                     <!-- <img src="../../Public/Images/German-Shepherd.jpg" alt="">
                     <img src="../../Public/Images/husky.jpg" alt="">
@@ -81,14 +91,14 @@
 	<!-- ADMIN PANEL -->
                 <td class="AdminPanel">
                     <div class="AdminPanelBox">
-                        <h1>WELCOME ADMIN</h1>
-                        <img src="../../Public/Images/avatars-pitbull-dogs-ears-512.png" alt="">
+                        <h1>WELCOME!</h1>
+                        <img src="../../Public/Images/login2.jpg" alt="">
                         <!-- ADMIN PANEL INFO -->
                         <div class="AdminPanelInfo">
-                            <h2>NAME- SWAROOP GOGATE</h2>
-                            <h4>LIKES - 6</h4>
-                            <h4>POSTS -3</h4>
-                            <h4>Friends - 8</h4>
+                            <h2>OWNER: SWAROOP GOGATE</h2>
+                            <h4>LIKES: 6</h4>
+                            <h4>POSTS: 3</h4>
+                            <h4>Friends: 8</h4>
                         </div>
                     </div>
 
