38be64343435cd1e1749832a26d078c047685f81
jerilmartinknows <67497699+jerilmartinknows@users.noreply.github.com>
2020-10-18 16:12:07 +0530

    userpage php and css
    userpage php and css

--- IPProject/views/Main/userpage.php
+++ IPProject/views/Main/userpage.php
@@ -9,27 +9,35 @@
     } else{
             
 ?>
+    <link rel="stylesheet" href="../../Public/Stylesheets/userpage.css">
+    <div class="wrapper">
+        <div class="box">
+            <div class="box-img">
+                <img src="pic5.jpg" height="625" width="500">
+            </div>
+            <div class="box-text">
+                <h1>BRUCE KHERDEKAR <a href="Edit"><i class="fas fa-edit"></i></a></h1>
+                <p>Breed: Labrador</p>
+                <p>Age: 5</p>
+                <p>Gender: Male</p>
+                <p>Cost: 10000000</p>
+                <p><i class="fas fa-map-marker-alt"></i> Mumbai</p>
 
 
-    <img src=" 
-        <?php
-            $user_obj = new User($con , $loggedin_user);
-            echo $user_obj->getProfilePic();
-        ?>
-     " alt="">    
-    <p></p>
-    <h2> Name : <?php echo $loggedin_user; ?> </h2> 
-    <h2>Likes : <?php
-            $user_obj = new User($con , $loggedin_user);
-            echo $user_obj->getNumLikes();
-        ?>
-    </h2>
+            </div>
+            <div class="social">
+                <a href="Instagram"><i class="fab fa-instagram"></i></a>
+                <a href="Facebook"><i class="fab fa-facebook-f"></i></a>
+                <a href="E-mail"><i class="far fa-envelope"></i></a>
+            </div>
+        </div>
+    </div>
+    
+    
 
-    <h5>ETC.</h5>
-
-    <button>
+    <button class="makeapost">
         <a href="./userpostform.php">Make a new post</a>
-    </button>
+    </button> 
 
 
 <!-- FOOTER -->
