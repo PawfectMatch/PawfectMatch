5f84b17398000d4c98d3761feb310083524265d6
SWAROOP-01 <61293095+SWAROOP-01@users.noreply.github.com>
2020-12-03 23:23:54 +0530

    Almost Done

--- IPProject/views/Posts/Bruce.php
+++ IPProject/views/Posts/Bruce.php
@@ -2,23 +2,26 @@
 <?php
     include("../../includes/partials/header.php");
     include("../../includes/classes/User.php");
-    $AllPosts = $con->query("SELECT * from posts");
-
+    $SinglePosts = $con->query("SELECT * from posts where post_id = '12'");
+    $data = mysqli_fetch_array($SinglePosts);
+    $owner_id = $data['owner_id'];
+    $userdata =    $con->query("SELECT * from users where user_id = '$owner_id'");
+    $data2 = mysqli_fetch_array($userdata);
+    
 ?>
 
 <link rel="stylesheet" href="../../Public/Stylesheets/PostBruce.css">
 
 <div class="somespace"></div>
 <div class="card">
-  <img src="" alt="John" style="width:100%">
-  <h1>Bruce Kherdekar</h1>
-  <h1>Age : 4</h1>
-  <h3>Description : Fluffy</h3>  
-  <a href="#"><i class="fa fa-dribbble"></i></a>
-  <a href="#"><i class="fa fa-twitter"></i></a>
-  <a href="#"><i class="fa fa-linkedin"></i></a>
-  <a href="#"><i class="fa fa-facebook"></i></a>
-  <p><button>Contact</button></p>
+  <img src="<?php echo $data['picofpet'] ?>" alt="John" style="width:100%">
+  <h1><?php echo $data['nameofpet'] ?></h1>
+  <h1>Age : <?php echo $data['ageofpet'] ?></h1>
+  <h3>Description : <?php echo $data['descr'] ?></h3>  
+
+  <p><button>Contact User</button></p>
+  <h2>User : <?php echo $data2['username'] ?></h2>
+  <h2>Email id : <?php echo $data2['email'] ?></h2>
 </div>
 
 
